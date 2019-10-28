53c80821ebf79f61223bd237d41d984116abfeeb
vonpessi <juhopessi1@gmail.com>
2019-10-28 18:06:41 +0200

    search from website and cleaning

--- grouplisting.py
+++ grouplisting.py
@@ -6,6 +6,7 @@ import sys
 import threading
 import csv
 import datetime
+from urllib.request import urlopen
 
 quitProgram = False
 
@@ -20,6 +21,16 @@ signal.signal(signal.SIGINT, signalHandler)
 x = 1
 
 
+def exitMessage():
+    print('\nThere must be a three argument after Python script.\n\n'
+          '$1 = list_of_urls\n'
+          '$2 = list_of_regular_expressions\n'
+          '$3 = csv_file_where_to_store_data\n\n'
+          'for example\n\n'
+          'python3 grouplisting.py example_list_of_urls.txt regex.csv data/grouptest.csv\n')
+    sys.exit()
+
+
 class grouping(threading.Thread):
 
     def __init__(self):
@@ -28,41 +39,56 @@ class grouping(threading.Thread):
 
     def run(self):
 
-        timeStamp = datetime.datetime.now()
+        if os.path.exists(sys.argv[1]):
 
-        # open url file and check each line of urls and remove unnecessary spaces
-        with open(sys.argv[1], 'r') as urlsFile:
-            for url in urlsFile:
-                newRow = [timeStamp, url.replace(' ', '').replace('\n', '')]
+            timeStamp = datetime.datetime.now()
 
-                t = threading.Thread(target=checkRegExFromUrl, args=(url, newRow))
-                t.start()
+            # open url file and check each line of urls and remove unnecessary spaces
+            with open(sys.argv[1], 'r') as urlsFile:
+                for url in urlsFile:
+                    newRow = [timeStamp, url.replace('\n', '')]
+
+                    # open url and decode it to utf8 format
+                    f = urlopen(url)
+                    bytes = f.read()
+                    urlText = bytes.decode("utf8")
+
+                    t = threading.Thread(target=checkRegExFromUrl, args=(urlText, newRow))
+                    t.start()
+        else:
+            print("file1 doesn't exist")
 
 
-def checkRegExFromUrl(url, newRow):
-    # open regex file and csv.reader
-    with open(sys.argv[2], 'r') as regExFile:
-        csv_reader = csv.reader(regExFile)
-        for regEx in csv_reader:
-            reCompiledRegEx = re.compile(regEx[0])
+def checkRegExFromUrl(urlText, newRow):
+    if os.path.exists(sys.argv[2]):
 
-            # check if the regex match  the url and gives boolean
-            match = re.search(reCompiledRegEx, url)
-            if match:
-                newRow.append(True)
-            else:
-                newRow.append(False)
+        # open regex file and csv.reader
+        with open(sys.argv[2], 'r') as regExFile:
+            csv_reader = csv.reader(regExFile)
+            for regEx in csv_reader:
+                reCompiledRegEx = re.compile(regEx[0])
 
-    # write new row to the csv file
-    with open(sys.argv[3], 'a') as f:
-        writer = csv.writer(f)
-        writer.writerow(newRow)
+                # check if the regex match  the url and gives boolean
+                match = re.findall(regEx[0], urlText)
+                if match:
+                    newRow.append(True)
+                else:
+                    newRow.append(False)
+
+        # write new row to the csv file
+        with open(sys.argv[3], 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow(newRow)
+
+    else:
+        print("file2 doesn't exist")
+        exitMessage()
 
 
 def writeHeader():
 
     if os.path.exists(sys.argv[3]):
-        return
+        print(sys.argv[3] + " exist")
     else:
         # write date and Url to the header
         csvFile = open(sys.argv[3], 'w')
@@ -78,22 +104,16 @@ def writeHeader():
                 else:
                     headerNameList.append(headerName[1])
 
-    writer = csv.DictWriter(
-        csvFile, headerNameList)
-    writer.writeheader()
-    csvFile.close()
+        writer = csv.DictWriter(
+            csvFile, headerNameList)
+        writer.writeheader()
+        csvFile.close()
 
 
 while True:
 
     if len(sys.argv) <= 3:
-        print('\nThere must be a three argument after Python script.\n\n'
-              '$1 = list_of_urls\n'
-              '$2 = list_of_regular_expressions\n'
-              '$3 = csv_file_where_to_store_data\n\n'
-              'for example\n\n'
-              'python3 grouplisting.py example_list_of_urls.txt regex.csv data/grouptest.csv\n')
-        exit(1)
+        exitMessage()
     else:
 
         writeHeader()
